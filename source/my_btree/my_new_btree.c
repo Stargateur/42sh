0ee81773b01611a980c5036c67b5c585047dabc3
gicque_p <gicque_p@epitech.eu>
2014-05-09 13:34:50 +0200

    Git de merde

---
+++ source/my_btree/my_new_btree.c
@@ -0,0 +1,25 @@
+/*
+** my_new_btree.c for  in /home/gicque_p/rendu/PSU_2013_42sh/source/my_btree
+** 
+** Made by Pierrick Gicquelais
+** Login   <gicque_p@epitech.net>
+** 
+** Started on  Fri May  9 13:29:54 2014 Pierrick Gicquelais
+** Last update Fri May  9 13:32:12 2014 Pierrick Gicquelais
+*/
+
+#include	<stdlib.h>
+#include	"my_btree.h"
+#include	"my_str.h"
+
+t_btree		*my_new_btree(void)
+{
+  t_btree	*btree;
+
+  if ((btree = my_malloc(sizeof(t_btree))) == NULL)
+    return (NULL);
+  btree->left = NULL;
+  btree->right = NULL;
+  btree->token = NULL;
+  return (btree)
+}
