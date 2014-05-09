995ccb3c6651977db2f24734630fed8617669b7f
plasko_a <plasko_a@epitech.eu>
2014-05-09 14:49:18 +0200

    pour neyden

---
+++ source/my_exec/my_exec.c
@@ -0,0 +1,12 @@
+/*
+** my_exec.c for my_exec in /home/plasko_a/rendu/PSU_2013_42sh/source/my_exec
+** 
+** Made by Antoine Plaskowski
+** Login   <antoine.plaskowski@epitech.eu>
+** 
+** Started on  Fri May  9 14:48:36 2014 Antoine Plaskowski
+** Last update Fri May  9 14:49:05 2014 Antoine Plaskowski
+*/
+
+#include	<stdlib.h>
+#include
