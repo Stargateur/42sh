3ff82d7d689d465f895be2635d3fc78e39e36292
gicque_p <gicque_p@epitech.eu>
2014-05-21 15:13:25 +0200

    -42 évité du malloc + début de my_editor

---
+++ my_editor/my_append_char.c
@@ -0,0 +1,26 @@
+/*
+** my_append_char.c for 42sh in /home/gicque_p/rendu/PSU_2013_42sh/my_editor
+** 
+** Made by Pierrick Gicquelais
+** Login   <gicque_p@epitech.net>
+** 
+** Started on  Tue May 20 17:05:33 2014 Pierrick Gicquelais
+** Last update Tue May 20 17:07:49 2014 Pierrick Gicquelais
+*/
+
+#include	<stdlib.h>
+#include	"my_editor.h"
+
+t_edit		*my_append_char(t_edit *old, char c)
+{
+  t_edit	*new;
+
+  if ((new = my_new_editor()) == NULL)
+    return (NULL);
+  if ((old = my_last_char(old)) != NULL)
+    old->next = new;
+  new->c = c;
+  new->prev = old;
+  new->next = NULL;
+  return (new);
+}
---
+++ my_editor/my_editor.h
@@ -0,0 +1,40 @@
+/*
+** my_editor.h for 42sh in /home/gicque_p/rendu/PSU_2013_42sh/my_editor
+** 
+** Made by Pierrick Gicquelais
+** Login   <gicque_p@epitech.net>
+** 
+** Started on  Tue May 20 17:00:16 2014 Pierrick Gicquelais
+** Last update Wed May 21 08:03:12 2014 Pierrick Gicquelais
+*/
+
+#ifndef		MY_EDITOR_H_
+# define	MY_EDITOR_H_
+
+# include	<termios.h>
+
+typedef	struct	s_edit	t_edit;
+
+struct		s_edit
+{
+  t_edit	*prev;
+  t_edit	*next;
+  char		c;
+};
+
+t_edit		*my_new_editor();
+t_edit		*my_first_char(t_edit *);
+t_edit		*my_last_char(t_edit *);
+t_edit		*my_append_char(t_edit *, char);
+int		my_putchar(char);
+int		raw_attr(struct termios *);
+int		canon_attr(struct termios *);
+int		is_printable(char);
+int		is_enter(char);
+int		is_delete(char);
+int		is_left(char *);
+int		is_right(char *);
+void		my_aff_editor(t_edit *);
+void		my_free_editor(t_edit *);
+
+#endif		/* !MY_EDITOR_H_ */
